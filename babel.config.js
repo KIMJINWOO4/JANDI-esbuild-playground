50cadd947f1ec71cf1d6da16f429ce76dacbc99f
kimjinwoo <rlawlsdn177@naver.com>
2024-12-24 17:17:25 +0900

    test: Bundler Test (Gluestack)

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
+  presets: [
+    'module:@react-native/babel-preset',
+    '@babel/preset-typescript',
+    'nativewind/babel',
+  ],
 
   plugins: [
     [
--- react-native-esbuild.config.js
+++ react-native-esbuild.config.js
@@ -1,6 +1,30 @@
+/**
+ * @type {import('@react-native-esbuild/core').Config}
+ */
+
 exports.default = {
+  cache: true,
+  logger: {
+    disabled: false,
+    timestamp: null,
+  },
+  resolver: {
+    mainFields: RESOLVER_MAIN_FIELDS,
+    sourceExtensions: SOURCE_EXTENSIONS,
+    assetExtensions: ASSET_EXTENSIONS,
+  },
   transformer: {
-    stripFlowPackageNames: ['react-native-runtime-transform-plugin'],
+    stripFlowPackageNames: [
+      'react-native',
+      'react-native-runtime-transform-plugin',
+    ],
+    jsc: {
+      transform: {
+        react: {
+          runtime: 'automatic',
+        },
+      },
+    },
     fullyTransformPackageNames: ['@react-native-aria'],
 
     additionalTransformRules: {
@@ -21,3 +45,66 @@ exports.default = {
     },
   },
 };
+
+/**
+ * @see {@link https://github.com/facebook/metro/blob/0.72.x/docs/Configuration.md#resolvermainfields}
+ */
+const RESOLVER_MAIN_FIELDS = ['react-native', 'browser', 'main', 'module'];
+
+/**
+ * @see {@link https://github.com/facebook/metro/blob/0.72.x/packages/metro-config/src/defaults/defaults.js}
+ * @see {@link https://github.com/facebook/metro/blob/0.72.x/packages/metro-file-map/src/workerExclusionList.js}
+ */
+const SOURCE_EXTENSIONS = [
+  '.tsx',
+  '.ts',
+  '.jsx',
+  '.js',
+  '.mjs',
+  '.cjs',
+  '.json',
+];
+
+const IMAGE_EXTENSIONS = [
+  '.bmp',
+  '.gif',
+  '.ico',
+  '.jpeg',
+  '.jpg',
+  '.png',
+  '.tiff',
+  '.tif',
+  '.webp',
+];
+
+const ASSET_EXTENSIONS = [
+  // Video extensions.
+  '.avi',
+  '.mp4',
+  '.mpeg',
+  '.mpg',
+  '.ogv',
+  '.webm',
+  '.3gp',
+  '.3g2',
+
+  // Audio extensions.
+  '.aac',
+  '.midi',
+  '.mid',
+  '.mp3',
+  '.oga',
+  '.wav',
+  '.3gp',
+  '.3g2',
+
+  // Font extensions.
+  '.eot',
+  '.otf',
+  '.ttf',
+  '.woff',
+  '.woff2',
+
+  // Image extensions.
+  ...IMAGE_EXTENSIONS,
+];
